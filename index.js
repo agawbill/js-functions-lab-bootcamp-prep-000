3848683b1860b48e97160fe55c4b98cc47120b78
Anthony Gawbill <gaw316@gmail.com>
2018-01-24 23:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return `Happy holidays, ${name}!'
 }
  
 
 function happyHolidaysTo(name, holiday) {
+  return `Ha${name}, '
   console.log(`Happy ${holiday}, ${name}!`)
 }
  
 function happyHolidayCountdown(days, holiday) {
   return `${days}, ${holiday}''
+}
+
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+  console.log('I was called!')
 }
