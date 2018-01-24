950fe9cfcff59b64bb02a32dbd781461dfe76292
Anthony Gawbill <gaw316@gmail.com>
2018-01-24 23:31:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "'Happy holidays, ${name}!'"
+  console.log('Happy holidays, ${name}!')
+  return 'Happy holidays, ${name}!'
 }
  
 
